26cdd7f16795c79d476d7cedd8663400f6ffbc67
U-CT\vkaloidis <vkaloidis@LAPVKALOIDIS.ct.apelon.com>
2015-10-16 16:00:16 -0400

    More work done on mediator

--- src/main/java/com/apelon/mediator/DefaultOrchestrator.java
+++ src/main/java/com/apelon/mediator/DefaultOrchestrator.java
@@ -3,16 +3,19 @@ package com.apelon.mediator;
 import java.util.HashMap;
 import java.util.Map;
 
-import akka.actor.ActorSelection;
-import akka.actor.UntypedActor;
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
-
 import org.apache.http.HttpStatus;
 import org.openhim.mediator.engine.MediatorConfig;
 import org.openhim.mediator.engine.messages.FinishRequest;
 import org.openhim.mediator.engine.messages.MediatorHTTPRequest;
 import org.openhim.mediator.engine.messages.MediatorHTTPResponse;
+
+import akka.actor.ActorSelection;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class DefaultOrchestrator extends UntypedActor {
 	LoggingAdapter log = Logging.getLogger(getContext().system(), this);
@@ -52,7 +55,7 @@ public class DefaultOrchestrator extends UntypedActor {
 		httpConnector.tell(serviceRequest, getSelf());
 	}
 	
-	private void processFacilityResponse(MediatorHTTPResponse response) {
+	private void processResponse(MediatorHTTPResponse response) {
 		log.info("Received response from Facility mediator service");
 		originalRequest.getRespondTo().tell(response.toFinishRequest(), getSelf());
 	}
@@ -60,26 +63,36 @@ public class DefaultOrchestrator extends UntypedActor {
 	@Override
 	public void onReceive(Object msg) throws Exception {
 		log.info("Accessing Default Orchestrator");
-//
-//		System.out.println("Here");	
-//	
-//		if (msg instanceof MediatorHTTPRequest) {
-//			queryFacility((MediatorHTTPRequest) msg);
-//		} else if (msg instanceof MediatorHTTPResponse) {
-//			processFacilityResponse((MediatorHTTPResponse) msg);
-//		} else {
-//			unhandled(msg);
-//		}
-		
-		//YEOMAN GENERATED METHOD
+
+		System.out.println("Here");	
+	
 		if (msg instanceof MediatorHTTPRequest) {
-			FinishRequest finishRequest = new FinishRequest(
-					"A message from my new mediator!", "text/plain",
-					HttpStatus.SC_OK);
-			((MediatorHTTPRequest) msg).getRequestHandler().tell(finishRequest,
-					getSelf());
+			queryFacility((MediatorHTTPRequest) msg);
+		} else if (msg instanceof MediatorHTTPResponse) {
+			if(((MediatorHTTPResponse) msg).getStatusCode() == HttpStatus.SC_OK) {
+				//TODO Finish this
+				//Gson gson = new GsonBuilder().create();
+				//FacilityRecord facilityRecord = gson.fromJson(((MediatorHTTPResponse) msg).getBody(), FacilityRecord.class);
+				
+				StringBuilder html = new StringBuilder("<html><body><h1>Facilities</h1></body></html>");
+				
+				FinishRequest fr = new FinishRequest(html.toString(), "text/html", HttpStatus.SC_OK);
+				
+				originalRequest.getRespondTo().tell(fr, getSelf());
+			}
 		} else {
 			unhandled(msg);
 		}
+		
+//		//YEOMAN GENERATED METHOD
+//		if (msg instanceof MediatorHTTPRequest) {
+//			FinishRequest finishRequest = new FinishRequest(
+//					"A message from my new mediator!", "text/plain",
+//					HttpStatus.SC_OK);
+//			((MediatorHTTPRequest) msg).getRequestHandler().tell(finishRequest,
+//					getSelf());
+//		} else {
+//			unhandled(msg);
+//		}
 	}
 }
